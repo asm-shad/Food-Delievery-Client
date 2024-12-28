f33e8c92aea870cd2fb7fc5ff4fa10800c300e2b
Asm Shad (INT) <asm.shad@liz.com>
2024-12-28 10:21:36 +0600

    Header design complete

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default Home;
