92ebe309520b346cdf07aec84d2c0d4d51e9a9f1
Asm Shad (INT) <asm.shad@liz.com>
2025-01-14 12:15:59 +0600

    pAYMENT Method using Stripe implemented

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,27 +1,85 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
-import { DataContext } from "../../context/dataContext";
+import { DataContext } from "../../context/DataContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(DataContext);
+  const { getTotalCartAmount, token, food_list, cartItems, url } =
+    useContext(DataContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+    };
+    let response = await axios.post(url + "/api/order/place", orderData, {
+      headers: { token },
+    });
+    if (response.data.success) {
+      const { session_url } = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+    }
+  };
+
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!token) {
+  //     navigate("/cart");
+  //   } else if (getTotalCartAmount() === 0) {
+  //     navigate("/cart");
+  //   }
+  // }, [token]);
+
   return (
-    <form className="place-order">
+    <form onSubmit={placeOrder} className="place-order">
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
           <input
             required
             name="firstName"
-            // onChange={onChangeHandler}
-            // value={data.firstName}
+            onChange={onChangeHandler}
+            value={data.firstName}
             type="text"
             placeholder="First Name"
           />
           <input
             required
             name="lastName"
-            // onChange={onChangeHandler}
-            // value={data.lastName}
+            onChange={onChangeHandler}
+            value={data.lastName}
             type="text"
             placeholder="Last Name"
           />
@@ -30,8 +88,8 @@ const PlaceOrder = () => {
           className="emaill"
           required
           name="email"
-          // onChange={onChangeHandler}
-          // value={data.email}
+          onChange={onChangeHandler}
+          value={data.email}
           type="email"
           placeholder="Email address"
         />
@@ -39,8 +97,8 @@ const PlaceOrder = () => {
           className="streett"
           required
           name="street"
-          // onChange={onChangeHandler}
-          // value={data.street}
+          onChange={onChangeHandler}
+          value={data.street}
           type="text"
           placeholder="Street"
         />
@@ -48,16 +106,16 @@ const PlaceOrder = () => {
           <input
             required
             name="city"
-            // onChange={onChangeHandler}
-            // value={data.city}
+            onChange={onChangeHandler}
+            value={data.city}
             type="text"
             placeholder="City"
           />
           <input
             required
             name="state"
-            // onChange={onChangeHandler}
-            // value={data.state}
+            onChange={onChangeHandler}
+            value={data.state}
             type="text"
             placeholder="State"
           />
@@ -66,26 +124,26 @@ const PlaceOrder = () => {
           <input
             required
             name="zipcode"
-            // onChange={onChangeHandler}
-            // value={data.zipcode}
+            onChange={onChangeHandler}
+            value={data.zipcode}
             type="text"
             placeholder="Zip code"
           />
           <input
             required
             name="country"
-            // onChange={onChangeHandler}
-            // value={data.country}
+            onChange={onChangeHandler}
+            value={data.country}
             type="text"
             placeholder="Country"
           />
         </div>
         <input
-          className="phone"
+          className="phonee"
           required
           name="phone"
-          // onChange={onChangeHandler}
-          // value={data.phone}
+          onChange={onChangeHandler}
+          value={data.phone}
           type="text"
           placeholder="Phone"
         />
