2029d914c9421e153b72aae489dceb11127e5b47
Asm Shad (INT) <asm.shad@liz.com>
2025-01-14 12:29:43 +0600

    Payment verification implemented

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -23,7 +23,7 @@ function Cart() {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div className="">
+              <div key={index} className="">
                 <div className="cart-items-title cart-items-item">
                   <img
                     src={
